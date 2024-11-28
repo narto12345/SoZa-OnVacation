61ed5b3aac32383c5af8b7249883cd3e7d4b26ea
Nicolas <nicosan12@hotmail.com>
2024-11-27 21:55:03 -0500

    added: Contact table added in model migration

--- PrincipalSite/models.py
+++ PrincipalSite/models.py
@@ -52,3 +52,14 @@ class Offer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
