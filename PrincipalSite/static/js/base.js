3722a5044e010511cf3b57dfbc3acb317cd1c331
Nicolas <nicosan12@hotmail.com>
2024-11-30 13:54:34 -0500

    nav admin options are added

---
+++ PrincipalSite/static/js/base.js
@@ -0,0 +1,24 @@
+// Nicolas Code
+const dropdownButton = document.getElementById("dropdown-button");
+const dropdownContent = document.getElementById("dropdown-content");
+const navbarCollapse = document.getElementById("navbarCollapse");
+
+dropdownButton.addEventListener("click", () => {
+  const isOpen = dropdownContent.style.opacity === "1";
+
+  if (isOpen) {
+    dropdownContent.style.opacity = "0";
+    dropdownContent.style.transform = "scaleY(0)";
+  } else {
+    dropdownContent.style.opacity = "1";
+    dropdownContent.style.transform = "scaleY(1)";
+  }
+});
+
+// Opcional: Cerrar el dropdown si haces clic fuera
+// document.addEventListener("click", (event) => {
+//   if (!event.target.closest(".dropdown")) {
+//     dropdownContent.style.opacity = "0";
+//     dropdownContent.style.transform = "scaleY(0)";
+//   }
+// });
