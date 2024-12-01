22ea043db73ac4aee9b836643842281f2ee4476f
Alecz <angiiealejandra22@gmail.com>
2024-12-01 08:21:13 -0500

    Page Contact

--- PrincipalSite/urls.py
+++ PrincipalSite/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns= [
 path('',views.InitialPage),
-path('login/', views.Login, name='login')
+path('login/', views.Login, name='login'),
+path('contact/',views.Contact, name='contact')
 ]
--- PrincipalSite/views.py
+++ PrincipalSite/views.py
@@ -8,3 +8,6 @@ def InitialPage (request):
 
 def Login (request):
     return render (request,'login.html')
+
+def Contact (request):
+    return render(request,'contact.html')
